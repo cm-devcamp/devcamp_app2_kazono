90e1abd7e2fba0120dbaa1c6bbb0ca760ee580f5
takipone <takipone@gmail.com>
2024-08-01 18:52:48 +0900

    ニュースページの内容を追加

--- app/news/[id]/page.tsx
+++ app/news/[id]/page.tsx
@@ -19,12 +19,16 @@ export async function generateStaticParams() {
 }
 
 export default function Page({ params }: { params: { id: string } }) {
+  const newsList: News[] = newsData;
+  const news = newsList.find((news) => params.id == news.id.toString()) ?? {'title':"Not found", 'description':"ニュースが見つかりませんでした"}
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-0">
       <Header />
       <div className="flex-col w-full min-h-screen max-w-5xl items-center justify-between p-6 bg-slate-50">
-        {/* News */}
-        News: {params.id}
+        <h1 className="text-4xl font-bold mb-6">News</h1>
+        <h2 className="text-2xl font-bold mb-6">{news.title}</h2>
+        <p>{news.description}</p>
       </div>
       <Footer />
     </main>
